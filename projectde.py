6615f9bb063e76a8a22e849d3e15a57105c6404a
vs-16 <vartikasahar@gmail.com>
2023-12-22 00:32:20 +0530

    Webpage integration done: code refractored

--- projectde.py
+++ projectde.py
@@ -11,28 +11,12 @@ import json
 import csv
 from dotenv import load_dotenv 
 import os
+import streamlit as st 
 
-def json_to_csv_conversion(data):
-    
-    electricity_data = data['response']['data']
- 
-    data_file = open('data_file.csv', 'w')
- 
-    csv_writer = csv.writer(data_file)
- 
-    count = 0
- 
-    for val in electricity_data:
-        if count == 0:
- 
-            header = val.keys()
-            csv_writer.writerow(header)
-            count += 1
- 
-        csv_writer.writerow(val.values())
- 
-    data_file.close()
 load_dotenv()
+
+st.title("Electricity Consumer count Prediction")
+
 api_key=os.getenv("api_key")
 headers={"authorization":api_key}
 params = {
@@ -47,41 +31,59 @@ params = {
 }
 
 r = requests.get("https://api.eia.gov/v2/electricity/state-electricity-profiles/net-metering/data/", params=params)
-json_to_csv_conversion(r.json())
-dt=pd.read_csv("data_file.csv")
+
+data=json.loads(r.content)
+ac_dt=data["response"]["data"]
+
+dt=pd.DataFrame(ac_dt)
 dt=dt.drop(["state","capacity-units","customers-units","sector"],axis=1)
 dt=dt.fillna(0)
+
 label_encoder= LabelEncoder()
 dt["stateName"]=label_encoder.fit_transform(dt["stateName"])
 dt["technology"]=label_encoder.fit_transform(dt["technology"])
 dt["sectorName"]=label_encoder.fit_transform(dt["sectorName"])
+
 x=dt[["stateName","technology","sectorName","capacity","period"]]
 y=dt["customers"]
-
 x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+
 model= LinearRegression()
 model.fit(x_train,y_train)
 y_pred=model.predict(x_test)
 mse= mean_squared_error(y_test,y_pred)
 r2= r2_score(y_test,y_pred)
 
+st_name=st.text_input(label="US State name")
+tech_name=st.text_input(label="Technology used for electricity production")
+sec_name=st.text_input(label="Sector consuming electricity")
+cap=st.number_input(label="Capacity of Electricity supplied (in Megawatts)")
+year=st.date_input(label="Enter the year")
+year=year.year
+btn=st.button(label="Submit")
+
 new_input = pd.DataFrame({
-    'stateName':["Arizona"] ,
-    'technology': ["All Technologies"],
-    'sectorName': ["commercial"],
-   'capacity': [1611.85],
-    'period': [2020]})
+    'stateName':[st_name] ,
+    'technology': [tech_name],
+    'sectorName': [sec_name],
+   'capacity': [cap],
+    'period': [year]})
+
 new_input["stateName"]=label_encoder.fit_transform(new_input["stateName"])
 new_input["technology"]=label_encoder.fit_transform(new_input["technology"])
 new_input["sectorName"]=label_encoder.fit_transform(new_input["sectorName"])
 pred_value= model.predict(new_input)
-print(np.floor(pred_value)[0])
 
-plt.scatter(y_test,y_pred)
-plt.xlabel("Actual num of customers")
-plt.ylabel("predicted num of customers")
-plt.title("Actual vs Predicted")
-regression_line= np.polyfit(y_test,y_pred,1)
-plt.plot(y_test,np.polyval(regression_line,y_test),color="Green",label="Reg line")
-plt.legend()
-plt.show()
+if btn and st_name!='' and tech_name!='' and sec_name!='': 
+    st.write("Predicted consumer count-")
+    pred=np.floor(pred_value)[0]
+    st.write(f'## {pred}') 
+    fig,ax=plt.subplots()
+    ax.scatter(y_test,y_pred)
+    ax.set_xlabel("Actual num of consumers")
+    ax.set_ylabel("Predicted num of consumers")
+    ax.set_title("Actual vs Predicted")
+    regression_line= np.polyfit(y_test,y_pred,1)
+    ax.plot(y_test,np.polyval(regression_line,y_test),color="Green",label="Reg line")
+    ax.legend()
+    st.pyplot(fig)
